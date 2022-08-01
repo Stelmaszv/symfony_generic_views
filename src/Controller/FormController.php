1502fe6a91428a81829957fa1ccf83d94697b927
Stelmaszv <stelmaszv@gmail.com>
2022-08-01 14:07:46 +0200

    Update in GenericFormController

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,9 +3,16 @@
 namespace App\Controller;
 
 use App\Generic\GenericFormController;
+use App\Generic\GenericSetDataInterFace;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Forms\Car;
 
-class FormController extends GenericFormController
+class FormController extends GenericFormController implements GenericSetDataInterFace
 {
+    public function setData(): void
+    {
+        $this->setForm(Car::class);
+        $this->setTwig('car/form.twig');
+    }
 }
--- src/Generic/GenericFormController.php
+++ src/Generic/GenericFormController.php
@@ -6,13 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form as SymfonyForm;
 use App\Forms\Car;
 
 class GenericFormController extends AbstractController
 {
+    private string $form='';
+
+    use Generic;
     public function form(Request $request): Response
     {
-        $form = $this->createForm(Car::class, null);
+        $this->setData();
+        $this->chcekData();
+        $form = $this->createForm($this->form, null);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()){
@@ -25,11 +31,32 @@ class GenericFormController extends AbstractController
         }else{
             $this->onSubmittedFalse();
         }
+        return $this->render($this->twing, $this->addAttributes($form));
+    }
 
-        return $this->render('car/form.twig', [
-            'controller_name' => 'GenericFormController',
+    private function chcekData() :void
+    {
+        if(!$this->form) {
+            throw new \Exception("form is not define in controller ".get_class($this)."!");
+        }
+
+        if(!$this->twing) {
+            throw new \Exception("Twing is not define in controller ".get_class($this)."!");
+        }
+    }
+
+    private function addAttributes(SymfonyForm $form) :array
+    {
+        
+        $this->attributes = [
             'form'=> $form->createView()
-        ]);
+        ];
+
+        return array_merge($this->attributes, $this->onSetAttribut());
+    }
+
+    protected function setForm(string $form){
+        $this->form= $form;
     }
 
     protected function onSubmittedTrue():void{}
