226a096bc962627d9ad1e3e6dddc802d6fec2706
Stelmaszv <stelmaszv@gmail.com>
2024-02-21 13:46:19 +0100

    Add APiCarUpdateController and ApiGenericUpdateController

---
+++ src/Controller/APiCarUpdateController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+use App\Generic\Api\ApiGenericUpdateController;
+
+class APiCarUpdateController extends ApiGenericUpdateController
+{
+
+}
---
+++ src/Generic/Api/ApiGenericUpdateController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Generic\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiGenericUpdateController extends AbstractController
+{
+
+}
